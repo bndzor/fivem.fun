944a850095f6b919a78afff23fd13e96c5e1195b
IceHax <vecchiotom1@gmail.com>
2018-05-27 23:16:40 +0200

    added resource to disable AI EMS and Police

---
+++ resources/disabledispatch/client.lua
@@ -0,0 +1,11 @@
+Citizen.CreateThread(function()
+	while true do
+		Wait(0)
+		for i = 1, 12 do
+			EnableDispatchService(i, false)
+		end
+		SetPlayerWantedLevel(PlayerId(), 0, false)
+		SetPlayerWantedLevelNow(PlayerId(), false)
+		SetPlayerWantedLevelNoDrop(PlayerId(), 0, false)
+	end
+end)
