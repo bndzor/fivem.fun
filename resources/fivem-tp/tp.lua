26a1dd7f645c2acfd7330e683885d94f608fe646
Emre Cürgül <pongo1999712@gmail.com>
2018-05-27 21:40:27 +0200

    fivem-tp uses menu now for waypoint tp

--- resources/fivem-tp/tp.lua
+++ resources/fivem-tp/tp.lua
@@ -2,20 +2,17 @@ RegisterCommand("tp", function(source, args, raw)
     SetEntityCoords(PlayerPedId(), args[1], args[2], args[3], 1, 0, 0, 1)
 end, false)
 
-Citizen.CreateThread(function()
-while true do
-    Wait(0)
-if IsControlJustPressed(1, 289) then
-    local blip = GetFirstBlipInfoId(8)
-    local blipX = 0.0
-    local blipY = 0.0
-    
-    if (blip ~= 0) then
-        local coord = GetBlipCoords(blip)
-        blipX = coord.x
-        blipY = coord.y
-    end
-    SetEntityCoords(PlayerPedId(),blipX,blipY,GetGroundZFor_3dCoord(blipX, blipY, 400.0),1,0,0,1)
-end
-end
+AddEventHandler("menu:setup", function()
+	TriggerEvent("menu:addModuleItem", nil, "Teleport To Waypoint", nil, false, function()
+		local blip = GetFirstBlipInfoId(8)
+		local blipX = 0.0
+		local blipY = 0.0
+		
+		if (blip ~= 0) then
+			local coord = GetBlipCoords(blip)
+			blipX = coord.x
+			blipY = coord.y
+		end
+		SetEntityCoords(PlayerPedId(),blipX,blipY,GetGroundZFor_3dCoord(blipX, blipY, 400.0),1,0,0,1)
+	end)
 end)
