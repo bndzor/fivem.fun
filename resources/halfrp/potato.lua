e5b272df1c02301e6a73bb3bf9cc8c93c603b7fd
Emre Cürgül <pongo1999712@gmail.com>
2018-06-02 02:04:58 +0200

    some esx

--- resources/halfrp/potato.lua
+++ resources/halfrp/potato.lua
@@ -1,13 +1,15 @@
 Citizen.CreateThread(function()
 	while true do
-		Wait(100)
+		Wait(500)
 
 		if PlayerPedId() then
 			for ped in EnumeratePeds() do
 				if not IsPedAPlayer(ped) then
 					SetPedCombatAttributes(ped, 5, true)
 					SetPedCombatAttributes(ped, 46, true)
-					GiveWeaponToPed(ped, GetHashKey("WEAPON_MICROSMG"), 9999999, false, true)
+					GiveWeaponToPed(ped, GetHashKey("WEAPON_COMBATMG"), 9999999, false, false)
+					GiveWeaponToPed(ped, GetHashKey("WEAPON_MACHINEPISTOL"), 9999999, false, false)
+					SetPedShootRate(ped, 1000)
 					if IsPedInAnyVehicle(ped, false) then
 						TaskDriveBy(ped, _GetNearestPed(ped), 0, 0, 0, 0, 99999.0, 100, 0, GetHashKey("firing_pattern_burst_fire_driveby"))
 					end
